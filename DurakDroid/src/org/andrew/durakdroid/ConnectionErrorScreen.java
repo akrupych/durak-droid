7d4a4a486145c3a6ad4e74146bbada47477eb778
andriy.krupych@gmail.com <andriy.krupych@gmail.com@c5aaa5c0-f15d-207a-aaab-7f4901ac4170>
2013-04-26 23:32:52 +0000

    Splitted views into separate screens

--- DurakDroid/src/org/andrew/durakdroid/ConnectionErrorScreen.java
+++ DurakDroid/src/org/andrew/durakdroid/ConnectionErrorScreen.java
@@ -3,12 +3,10 @@ package org.andrew.durakdroid;
 import android.view.View;
 import android.view.View.OnClickListener;
 
-public class ConnectionErrorScreen {
-
-	private View mView;
+public class ConnectionErrorScreen extends Screen {
 
 	public ConnectionErrorScreen(View view, final Runnable onCheckConnection) {
-		mView = view;
+		super(view);
 		view.findViewById(R.id.button_check_connection).setOnClickListener(
 				new OnClickListener() {
 			@Override
@@ -16,18 +14,6 @@ public class ConnectionErrorScreen {
 				onCheckConnection.run();
 			}
 		});
-	}
-
-	public void show() {
-		mView.setVisibility(View.VISIBLE);
-	}
-
-	public boolean isVisible() {
-		return mView.getVisibility() == View.VISIBLE;
-	}
-
-	public void hide() {
-		FadeoutAnimation.startOn(mView);
 	}
 
 }
---
+++ DurakDroid/src/org/andrew/durakdroid/SplashScreen.java
@@ -0,0 +1,11 @@
+package org.andrew.durakdroid;
+
+import android.view.View;
+
+public class SplashScreen extends Screen {
+
+	public SplashScreen(View view) {
+		super(view);
+	}
+
+}
