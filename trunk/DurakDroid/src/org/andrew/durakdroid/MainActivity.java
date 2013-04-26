c69f4494bb7a9805d24fcea8fb832e44e4295bf6
andriy.krupych@gmail.com <andriy.krupych@gmail.com@c5aaa5c0-f15d-207a-aaab-7f4901ac4170>
2013-04-26 16:18:37 +0000

    Added WebClient

--- trunk/DurakDroid/src/org/andrew/durakdroid/MainActivity.java
+++ trunk/DurakDroid/src/org/andrew/durakdroid/MainActivity.java
@@ -1,17 +1,32 @@
 package org.andrew.durakdroid;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
 
-    @Override
+    protected static final String TAG = MainActivity.class.getName();
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         WebView webView = (WebView) findViewById(R.id.webView);
         webView.loadUrl("http://durak.time2play.mobi/landing");
+        webView.setWebViewClient(new WebViewClient() {
+        	@Override
+        	public void onPageStarted(WebView view, String url, Bitmap favicon) {
+        		Log.d(TAG, "onPageStarted: " + url);
+        	}
+        	@Override
+        	public void onPageFinished(WebView view, String url) {
+        		Log.d(TAG, "onPageFinished: " + url);
+        	}
+        });
     }
     
 }
