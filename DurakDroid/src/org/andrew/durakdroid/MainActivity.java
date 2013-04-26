cdab860fc4cdec4b054e04a8678715ef8eee45ed
andriy.krupych@gmail.com <andriy.krupych@gmail.com@c5aaa5c0-f15d-207a-aaab-7f4901ac4170>
2013-04-26 22:55:15 +0000

    Improved error message appearing

--- DurakDroid/src/org/andrew/durakdroid/MainActivity.java
+++ DurakDroid/src/org/andrew/durakdroid/MainActivity.java
@@ -1,22 +1,23 @@
 package org.andrew.durakdroid;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.view.animation.Animation.AnimationListener;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     protected static final String TAG = MainActivity.class.getName();
+
+	private ConnectivityManager mConnManager;
+    private ConnectionErrorScreen mErrorScreen;
     
     private boolean mIsLoaded = false;
 
@@ -24,8 +25,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mConnManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         final View splashScreen = findViewById(R.id.splash_screen);
-        final View errorScreen = findViewById(R.id.error_screen);
         final WebView webView = (WebView) findViewById(R.id.web_view);
         webView.loadUrl("http://durak.time2play.mobi/landing");
         webView.setWebViewClient(new WebViewClient() {
@@ -37,33 +38,14 @@ public class MainActivity extends Activity {
         	public void onPageFinished(WebView view, String url) {
         		Log.d(TAG, "onPageFinished: " + url);
         		if (!mIsLoaded) {
-            		splashScreen.postDelayed(new Runnable() {
-						@Override
-						public void run() {
-		        			Animation fadeout = new AlphaAnimation(1, 0);
-		        			fadeout.setDuration(1000);
-		        			fadeout.setAnimationListener(new AnimationListener() {
-								@Override
-								public void onAnimationStart(Animation animation) { }
-								@Override
-								public void onAnimationRepeat(Animation animation) { }
-								@Override
-								public void onAnimationEnd(Animation animation) {
-									splashScreen.setVisibility(View.GONE);
-								}
-							});
-							splashScreen.startAnimation(fadeout);
-						}
-					}, 1000);
+        			FadeoutAnimation.startOn(splashScreen);
         			mIsLoaded = true;
         		}
-        	}
-        	@Override
-        	public void onReceivedError(WebView view, int errorCode,
-        			String description, String failingUrl) {
-        		Log.e(TAG, "onReceivedError: " + description);
-        		errorScreen.setVisibility(View.VISIBLE);
-        		errorScreen.setTag(failingUrl);
+        		if (mConnManager.getActiveNetworkInfo() == null) {
+        			mErrorScreen.show();
+        		} else if (mErrorScreen.isVisible()) {
+        			mErrorScreen.hide();
+        		}
         	}
         	@Override
         	public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -75,14 +57,14 @@ public class MainActivity extends Activity {
         		return false;
         	}
         });
-        Button checkConnection = (Button) findViewById(R.id.button_check_connection);
-        checkConnection.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				errorScreen.setVisibility(View.GONE);
-				webView.loadUrl(errorScreen.getTag().toString());
-			}
-		});
+        mErrorScreen = new ConnectionErrorScreen(
+        		findViewById(R.id.error_screen),
+        		new Runnable() {
+					@Override
+					public void run() {
+						webView.reload();
+					}
+				});
     }
     
 }
