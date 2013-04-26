55ec7c6fcca0d756f27d9c8fabb0881583df4fd3
andriy.krupych@gmail.com <andriy.krupych@gmail.com@c5aaa5c0-f15d-207a-aaab-7f4901ac4170>
2013-04-26 20:29:51 +0000

    Added splash screen

--- trunk/DurakDroid/src/org/andrew/durakdroid/MainActivity.java
+++ trunk/DurakDroid/src/org/andrew/durakdroid/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -15,7 +16,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WebView webView = (WebView) findViewById(R.id.webView);
+        final View splashScreen = findViewById(R.id.splash_screen);
+        WebView webView = (WebView) findViewById(R.id.web_view);
         webView.loadUrl("http://durak.time2play.mobi/landing");
         webView.setWebViewClient(new WebViewClient() {
         	@Override
@@ -25,6 +27,12 @@ public class MainActivity extends Activity {
         	@Override
         	public void onPageFinished(WebView view, String url) {
         		Log.d(TAG, "onPageFinished: " + url);
+        		splashScreen.setVisibility(View.GONE);
+        	}
+        	@Override
+        	public void onReceivedError(WebView view, int errorCode,
+        			String description, String failingUrl) {
+        		Log.e(TAG, "onReceivedError: " + description);
         	}
         });
     }
