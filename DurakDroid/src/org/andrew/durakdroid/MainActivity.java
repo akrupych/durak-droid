4cc6767839bb827514a5a5cb54c1327e57cf807e
andriy.krupych@gmail.com <andriy.krupych@gmail.com@c5aaa5c0-f15d-207a-aaab-7f4901ac4170>
2013-04-26 16:10:04 +0000

    Project setup

---
+++ DurakDroid/src/org/andrew/durakdroid/MainActivity.java
@@ -0,0 +1,17 @@
+package org.andrew.durakdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.loadUrl("http://durak.time2play.mobi/landing");
+    }
+    
+}
